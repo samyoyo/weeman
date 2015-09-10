3481e24243060289852af532c92a2458a1a5ddeb
Hypsurus <Hypsurus@users.noreply.github.com>
2015-09-10 05:03:47 +0300

    Weeman 1.0

---
+++ README.md
@@ -0,0 +1,32 @@
+Weeman - http server for phishing
+=================================
+
+About
+=====
+
+Usually Weeman will run along side MITM attack.
+
+![Weeman](https://pbs.twimg.com/media/COftz7qVAAELhct.jpg)
+
+Requirements
+============
+
+* python 2.7
+* python2- BeautifulSoup 
+
+Usage
+======
+
+Just run `python2 ./weeman` and you will dropped in the shell.
+
+type help for usage.
+
+Get Weeman
+=============
+                git clone git://github.com/Hypsurus/weeman
+
+The END
+=========
+    
+    Copyright 2015 (C) Hypsurus <hypsurus@mail.ru>.
+    License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
---
+++ core/misc.py
@@ -0,0 +1,30 @@
+##
+## misc.py - usefull functions
+##
+
+import sys
+import time
+
+def printt(s, msg):
+    if s == 1:
+        print("\033[01;31m[%s] Error: %s\033[00m" %(time.strftime("%H:%M:%S"),msg))
+        sys.exit(1)
+    elif s == 2:
+        print("\033[01;32m[%s] %s\033[00m" %(time.strftime("%H:%M:%S"),msg))
+    else:
+    
+        print("\033[01;01m[%s] %s\033[00m" %(time.strftime("%H:%M:%S"),msg))
+
+def print_help():
+    print("\tshow   : show default settings.")
+    print("\tset    : set settings (set port 80).")
+    print("\trun    : start the server.")
+    print("\tclear  : clear screen.")
+    print("\thelp   : show help.")
+    print("\tquit   : bye bye.")
+
+def isroot():
+    if os.getuid() !=0:
+        printt(1,"Please run weeman as root.")
+
+
